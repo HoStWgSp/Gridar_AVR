c741d4c55bbadd9d15fd6c9fb19bff4f37818d9e
HoStWgSp <gridar@outlook.com>
2022-11-30 18:49:23 +1100

    Подготовил програмку для запиливания модулей

---
+++ Gridar_Lib/main.asm
@@ -0,0 +1,15 @@
+/*
+* AVRGridar_V.11.22.4
+*/
+
+.include "Main/Libs.inc"
+///////////////////////////////////////////////////////////////////////
+Reset:
+.include "Reset.inc"
+
+Proga:
+// Читайте README файл
+// Ваша программа
+rjmp Proga
+
+
