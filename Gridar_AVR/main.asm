ac709b49f7d67d569cff4efcc025549de86f4e5d
HoStWgSp <gridar@outlook.com>
2022-12-24 15:53:25 +1100

    GridarAVR V.3.01.12.22
    Запилил модуль для работы с DALLAS DS1820, DS18B20, DS18S20, DS1822

--- Gridar_AVR/main.asm
+++ Gridar_AVR/main.asm
@@ -1,5 +1,5 @@
 /*
-* AVRGridar_V.2.01.12.22
+* GridarAVR V.3.01.12.22
 */
 
 .include "Main/Libs.inc"
@@ -14,10 +14,10 @@ rjmp Proga
 
 LCD_TWSI_First_Page:
 	//message: .db $0b, 'T', 'e', 'm', 'p', 'e', 'r', 't', 'u', 'r', 'e', ':'
-	message1: .db $0c, 'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!', ' '
-	TWSI_START TWSI_Address, 'W'
-	HD44780U_Set_Cursor 2, 1
-	HD44780U_Write_cseg_String message1
+	//message1: .db $0c, 'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!', ' '
+	//TWSI_START TWSI_Address, 'W'
+	//HD44780U_Set_Cursor 2, 1
+	//HD44780U_Write_cseg_String message1
 	//HD44780U_Set_Cursor 2, 15
 	//HD44780U_Write_Symbol $2e
 	//HD44780U_Set_Cursor 2, 18
@@ -27,5 +27,5 @@ LCD_TWSI_First_Page:
 	//HD44780U_Cursor_visible disabled
 
 	//HD44780U_LCD_LED enabled
-	TWSI_STOP
+	//TWSI_STOP
 ret
