bd57a766bb110c92291d2af72e5a75ef2cfefa82
HoStWgSp <gridar@outlook.com>
2022-12-24 11:45:56 +1100

    Первый коммит. Подготовка к работе

---
+++ main.asm
@@ -0,0 +1,13 @@
+/*
+* AVRGridar_V.11.22.4
+*/
+
+.include "Main/Libs.inc"
+///////////////////////////////////////////////////////////////////////
+Reset:
+.include "Reset.inc"
+
+Proga:
+// ??????? README ????
+// ???? ?????????
+rjmp Proga
